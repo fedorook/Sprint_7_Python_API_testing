6999ad07bae8f6132e5cb301229285c0d28e9179
Fedorook <fedorook@gmail.com>
2025-05-03 22:49:17 +0800

    Moved payload creation to the Utils file

--- tests/test_courier_creation.py
+++ tests/test_courier_creation.py
@@ -2,26 +2,19 @@ import requests
 import random
 from conftest import courier_data
 from config import BASE_URL, COURIER
+from utils import make_courier_payload
 
 
 class TestCourierCreation:
-    def test_create_courier_success(self, courier_data):
-        data = {
-            "login": 'test_login_' + str(random.randint(1, 100000)),
-            "password": 'test_password_' + str(random.randint(1, 100000)),
-            "firstName": 'test_name_' + str(random.randint(1, 100000))
-        }
-        response = requests.post(f"{BASE_URL}{COURIER}", data=data)
+    def test_create_courier_success(self):
+        payload = make_courier_payload()
+        response = requests.post(f"{BASE_URL}{COURIER}", data=payload)
         assert response.status_code == 201
         assert response.json() == {"ok": True}
 
     def test_create_duplicate_courier(self, courier_data):
         login, password, first_name = courier_data
-        payload = {
-            "login": login,
-            "password": password,
-            "firstName": first_name
-        }
+        payload = make_courier_payload(login, password, first_name)
         response = requests.post(f"{BASE_URL}{COURIER}", data=payload)
         assert response.status_code == 409
         assert response.json()['message'] == "Этот логин уже используется. Попробуйте другой."
